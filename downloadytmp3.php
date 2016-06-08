bd01c1bc9b7df136eba33e45a46b3bbcadba3893
arianne-n <arianne-n@hotmail.com>
2016-06-08 17:03:21 +0200

    youtube to mp3 niet helemaal werkend

--- downloadytmp3.php
+++ downloadytmp3.php
@@ -2,16 +2,16 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+<h1>Youtube Downloader</h1>
 
 <?php
-	echo "Hello World!";
-	$cmd1 = 'youtube-dl'
-	exec($cmd1) or die("Unable to execute youtube-dl");
-	$cmd2 = 'youtube-dl --extract-audio --audio-format mp3 -o "./mp3files/%(title)s.%(ext)s"';
-	exec($cmd2) or die("unable to execute mp3 dowloading");
+	ini_set('display_errors',1);
+	error_reporting(E_ALL);
+    $yturl = 'https://www.youtube.com/watch?v=v04H7_fFC90';
+	$cmd = 'youtube-dl --extract-audio --audio-format mp3 -o "./mp3files/%(title)s.%(ext)s" ' . $yturl;
+	exec($cmd, $output) or die("cannot download video using youtube-dl");
+    var_export($output);
 ?>
 
 </body>
-<!-- --output '/mp3files/%(title)s.%(ext)s' -->
 </html>
