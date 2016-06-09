a63e1b2a7a9ab6884116f2bfee674c4274006f58
clara2911 <clara.tump@hotmail.com>
2016-06-09 11:19:05 +0200

    love

--- downloadytmp3.php
+++ downloadytmp3.php
@@ -7,10 +7,10 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(E_ALL);
-    $yturl = 'https://www.youtube.com/watch?v=v04H7_fFC90';
-	$cmd = 'youtube-dl --extract-audio --audio-format mp3 -o "./mp3files/%(title)s.%(ext)s" https://www.youtube.com/watch?v=v04H7_fFC90';
-	exec($cmd, $output) or die("cannot download video using youtube-dl");
-    var_export($output);
+    /*$yturl = 'https://www.youtube.com/watch?v=v04H7_fFC90';*/
+	$cmd = 'youtube-dl --extract-audio --audio-format mp3 --output "./mp3files/%(title)s.%(ext)s" https://www.youtube.com/watch?v=lO6FP83HAXk';
+	exec($cmd) or die("cannot download video using youtube-dl");
+    /*var_export($output);*/
 ?>
 
 </body>
