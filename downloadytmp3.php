eedd10071c182933e7c04aeb044ffe1d49211882
arianne-n <arianne-n@hotmail.com>
2016-06-09 11:28:07 +0200

    youtube download to mp3

--- downloadytmp3.php
+++ downloadytmp3.php
@@ -5,12 +5,9 @@
 <h1>Youtube Downloader</h1>
 
 <?php
-	ini_set('display_errors',1);
-	error_reporting(E_ALL);
-    /*$yturl = 'https://www.youtube.com/watch?v=v04H7_fFC90';*/
-	$cmd = 'youtube-dl --extract-audio --audio-format mp3 --output "./mp3files/%(title)s.%(ext)s" https://www.youtube.com/watch?v=lO6FP83HAXk';
+    $yturl = 'https://www.youtube.com/watch?v=eHJ7An2CMB4';
+	$cmd = 'youtube-dl --extract-audio --audio-format mp3 --output "./mp3files/%(title)s.%(ext)s" ' . $yturl;
 	exec($cmd) or die("cannot download video using youtube-dl");
-    /*var_export($output);*/
 ?>
 
 </body>
