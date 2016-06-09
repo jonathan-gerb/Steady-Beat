664cecd3ccbc3c935044e91456144b6194cd848f
arianne-n <arianne-n@hotmail.com>
2016-06-09 11:34:32 +0200

    now with id instead of title

--- downloadytmp3.php
+++ downloadytmp3.php
@@ -5,8 +5,8 @@
 <h1>Youtube Downloader</h1>
 
 <?php
-    $yturl = 'https://www.youtube.com/watch?v=eHJ7An2CMB4';
-	$cmd = 'youtube-dl --extract-audio --audio-format mp3 --output "./mp3files/%(title)s.%(ext)s" ' . $yturl;
+    $yturl = 'https://www.youtube.com/watch?v=v04H7_fFC90';
+	$cmd = 'youtube-dl --extract-audio --audio-format mp3 --output "./mp3files/%(id)s.%(ext)s" ' . $yturl;
 	exec($cmd) or die("cannot download video using youtube-dl");
 ?>
 
