d55a080e901b07a688b8f7fbb7c69e2b1bf1290e
Adriaan de Vries <adriaan.de.vries@hotmail.com>
2016-06-10 15:56:40 +0200

    improved song

--- downloadytwav.php
+++ downloadytwav.php
@@ -13,7 +13,7 @@ error_reporting(E_ALL); */
 		$cmd = 'youtube-dl --extract-audio --audio-format wav --output "./mp3files/%(id)s.%(ext)s" ' . $yturl;
 		exec($cmd) or die("cannot download video using youtube-dl");
 	}	else {
-		$yturl = 'https://www.youtube.com/watch?v=VnRP5aw7mD8';
+		$yturl = 'https://www.youtube.com/watch?v=Qpl7XU9sLfs';
 		$cmd = 'youtube-dl --extract-audio --audio-format wav --output "./mp3files/FAIL%(id)s.%(ext)s" ' . $yturl;
 		exec($cmd) or die("cannot download video using youtube-dl");
 
