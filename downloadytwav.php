5ab8bc2965839db541645a9b1fce590148d8970d
clara2911 <clara.tump@hotmail.com>
2016-06-10 15:51:36 +0200

    youtube download verder

--- downloadytwav.php
+++ downloadytwav.php
@@ -6,23 +6,20 @@
 
 <?php
 /*ini_set('display_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); */
 
-	echo "HELLO WORLD";
-	if(isset($_POST['ytID']))
-	{
-	    $yturl = 'https://www.youtube.com/watch?v=' . $_POST['ytID'];
-		echo ytID;
-		$cmd = 'youtube-dl --extract-audio --audio-format mp3 --output "./mp3files/%(id)s.%(ext)s" ' . $yturl;
+	if(isset($_GET['ytID'])) {
+		$yturl = 'https://www.youtube.com/watch?v=VnRP5aw7mD8';
+		$cmd = 'youtube-dl --extract-audio --audio-format wav --output "./mp3files/%(id)s.%(ext)s" ' . $yturl;
 		exec($cmd) or die("cannot download video using youtube-dl");
-	} else { 
-		echo 'FAIL';
-	} */
+	}	else {
+		$yturl = 'https://www.youtube.com/watch?v=VnRP5aw7mD8';
+		$cmd = 'youtube-dl --extract-audio --audio-format wav --output "./mp3files/FAIL%(id)s.%(ext)s" ' . $yturl;
+		exec($cmd) or die("cannot download video using youtube-dl");
 
-$yturl = 'https://www.youtube.com/watch?v=R2F_hGwD26g';
-$cmd = 'youtube-dl --extract-audio --audio-format wav --output "./mp3files/%(id)s.%(ext)s" ' . $yturl;
-exec($cmd) or die("cannot download video using youtube-dl");
-?>
+	}
+
+?>	
 
 </body>
 </html>
