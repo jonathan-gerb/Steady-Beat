cd893215adefc3c19e7a7a4c1c35a382d6be65dc
BartLaan <bartlaan96@gmail.com>
2016-06-16 14:01:36 +0200

    removed a bunch of obsolete files

--- downloadytwav.php
+++ downloadytwav.php
@@ -11,11 +11,11 @@ error_reporting(E_ALL); */
 	if(isset($_POST['ID'])) {
         $ID = $_POST['ID'];
 		$yturl = 'https://www.youtube.com/watch?v=' . $ID;
-		$cmd = 'youtube-dl --extract-audio --audio-format wav --output "./mp3files/%(id)s.%(ext)s" ' . $yturl;
+		$cmd = 'youtube-dl --extract-audio --audio-format wav --output "./wavfiles/%(id)s.%(ext)s" ' . $yturl;
 		exec($cmd) or die("cannot download video using youtube-dl");
 	}	else {
 		$yturl = 'https://www.youtube.com/watch?v=Qpl7XU9sLfs';
-		$cmd = 'youtube-dl --extract-audio --audio-format wav --output "./mp3files/FAIL%(id)s.%(ext)s" ' . $yturl;
+		$cmd = 'youtube-dl --extract-audio --audio-format wav --output "./wavfiles/FAIL%(id)s.%(ext)s" ' . $yturl;
 		exec($cmd) or die("cannot download video using youtube-dl");
 
 	}
