2dc2e11cf031027b3902e8d62f72a2c14a7af573
BartLaan <bartlaan96@gmail.com>
2016-06-16 15:47:36 +0200

    overstap naar mp3

--- downloadytmp3.php
+++ downloadytmp3.php
@@ -5,9 +5,21 @@
 <h1>Youtube Downloader</h1>
 
 <?php
-    $yturl = 'https://www.youtube.com/watch?v=R2F_hGwD26g';
-	$cmd = 'youtube-dl --extract-audio --audio-format mp3 --output "./mp3files/%(id)s.%(ext)s" ' . $yturl;
-	exec($cmd) or die("cannot download video using youtube-dl");
+/*ini_set('display_errors', 1);
+error_reporting(E_ALL); */
+
+	if(isset($_POST['ID'])) {
+        $ID = $_POST['ID'];
+		$yturl = 'https://www.youtube.com/watch?v=' . $ID;
+		$cmd = 'youtube-dl --extract-audio --audio-format mp3 --output "./mp3files/%(id)s.%(ext)s" ' . $yturl;
+		exec($cmd) or die("cannot download video using youtube-dl");
+	}	else {
+		$yturl = 'https://www.youtube.com/watch?v=Qpl7XU9sLfs';
+		$cmd = 'youtube-dl --extract-audio --audio-format mp3 --output "./mp3files/FAIL%(id)s.%(ext)s" ' . $yturl;
+		exec($cmd) or die("cannot download video using youtube-dl");
+
+	}
+
 ?>
 
 </body>
