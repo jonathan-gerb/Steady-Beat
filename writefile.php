9c18ab2758e5983c92d03d8ba9768dc91cafa34d
BartLaan <bartlaan96@gmail.com>
2016-06-21 13:21:05 +0200

    time sigs upload

--- writefile.php
+++ writefile.php
@@ -9,13 +9,13 @@
 	    $beat = $_POST['beat'];
 	    $ytID = $_POST['ytID'];
 		$annotations = fopen("./txtfiles/" . $ytID . "-" . time() . ".txt", "w+") or die("Unable to open file!");
-		$beatArray = explode(",", "$beat");
-		foreach ($beatArray as $line){
-		    fwrite($annotations, $line . "\n");
+		$regionArray = explode(",", "$beat");
+		foreach ($regionArray as $line){
+			fwrite($annotations, $line . "\n");
 		}
 		fclose($annotations);
-		$wavFile = "./mp3files/" . $ytID . ".wav";
-		unlink($wavFile);
+		$mp3File = "./mp3files/" . $ytID . ".mp3";
+		unlink($mp3File);
 
 	} else {
 		echo "No beats and youtube ID received.";
