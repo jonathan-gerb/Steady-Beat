ca4afc758535c422963973c76c9f36cff50182e6
jonathan gerbscheid <jonathan-gerb@hotmail.com>
2016-06-21 14:09:29 +0200

    still working on autocomp

--- autocomplete.php
+++ autocomplete.php
@@ -7,16 +7,21 @@
 	if(isset($_POST['youtubeId']))
 	{
 	    $ID = $_POST['youtubeId'];
-		$command = "./vamp/release/sonic-annotator -d vamp:qm-vamp-plugins:qm-onsetdetector:onsets ./mp3files/lETmskoqh30.mp3 -w csv --csv-one-file ./onsets/ testy.csv";
-		exec($command);
-		exec("copy NUL EMptyFile.txt");
+		$command = "/vamp/release/sonic-annotator -d vamp:qm-vamp-plugins:qm-onsetdetector:onsets ./mp3files/" . $ID . ".mp3 -w  csv --csv-basedir ./onsets/ --force";
+		exec($command); //or echo "hallloooooooo clara";
+		exec("touch ./onsets/jonafile.txt");
+		$myfile = fopen("./onsets/testfile.txt", "w");
+		echo "whaaaaaaaaaaaaaaaaaaaaaaaa";
+		fclose($myfile);
 		echo "direct na command";
+
 	} else {
 		echo "No youtube ID received.";
 	}
 	echo "eind php";
 	echo $command;
+	echo $out;
 ?>
-hallooooooo skajfd;lskajdf;lasdkf
+he adriaan
 </body>
 </html>
