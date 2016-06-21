2b496e0ac29c6a4e945bd085235d24b31637b184
jonathan gerbscheid <jonathan-gerb@hotmail.com>
2016-06-21 17:20:04 +0200

    working on autocomplete

--- autocomplete.php
+++ autocomplete.php
@@ -7,9 +7,10 @@
 	if(isset($_POST['youtubeId']))
 	{
 	    $ID = $_POST['youtubeId'];
-		$command = "/vamp/release/sonic-annotator -d vamp:qm-vamp-plugins:qm-onsetdetector:onsets ./mp3files/" . $ID . ".mp3 -w  csv --csv-basedir ./onsets/ --force";
-		exec($command); //or echo "hallloooooooo clara";
-		exec("touch ./onsets/jonafile.txt");
+		exec("touch ./onsets/lETmskoqh30_vamp_qm-vamp-plugins_qm-onsetdetector_onsets.csv");
+		chmod("./onsets/lETmskoqh30_vamp_qm-vamp-plugins_qm-onsetdetector_onsets.csv", 0777);
+		$command = "./vamp/release/sonic-annotator.exe -d vamp:qm-vamp-plugins:qm-onsetdetector:onsets ./mp3files/" . $ID . ".mp3 -w  csv --csv-basedir ./onsets/ --force";
+		exec("./vamp/release/sonic-annotator.exe -d vamp:qm-vamp-plugins:qm-onsetdetector:onsets ./mp3files/" . $ID . ".mp3 -w  csv --csv-basedir ./onsets/ --force"); //or echo "hallloooooooo clara";
 		$myfile = fopen("./onsets/testfile.txt", "w");
 		echo "whaaaaaaaaaaaaaaaaaaaaaaaa";
 		fclose($myfile);
