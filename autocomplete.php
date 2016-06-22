30f48f28f5a1ee8e545b1ecb53f2fe1e3efd8e24
BartLaan <bartlaan96@gmail.com>
2016-06-22 23:52:17 +0200

    wavfiles ftw, help added, minor design things

--- autocomplete.php
+++ autocomplete.php
@@ -9,7 +9,7 @@
 	    $ID = $_POST['youtubeId'];
 		// exec("touch ./onsets/lETmskoqh30_vamp_qm-vamp-plugins_qm-onsetdetector_onsets.csv");
 		// chmod("./onsets/lETmskoqh30_vamp_qm-vamp-plugins_qm-onsetdetector_onsets.csv", 0777);
-		$command = "sonic-annotator -d vamp:qm-vamp-plugins:qm-onsetdetector:onsets ./mp3files/" . $ID . ".mp3 -w  csv --csv-basedir ./onsets/ --force";
+		$command = "sonic-annotator -d vamp:qm-vamp-plugins:qm-onsetdetector:onsets ./wavfiles/" . $ID . ".wav -w  csv --csv-basedir ./onsets/ --force";
 		exec($command); //or echo "hallloooooooo clara";
 	} else {
 		echo "No youtube ID received.";
